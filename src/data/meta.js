2e757be491b57d8afba51baa170b6f5aace2df5c
James Lovallo <lovallo.james@gmail.com>
2025-03-20 22:13:59 -0400

    update site meta

--- src/data/meta.js
+++ src/data/meta.js
@@ -1,16 +1,16 @@
 export default {
-  title: "Bounce House Kings",
-  description: "Make your next party magical.",
-  url: "https://bernadetteatuahene.com",
-  image: "/src/images/global/meta.jpeg",
-  links: [
-    {
-      rel: "icon",
-      href: "/src/images/global/favicon.png",
-    },
-    {
-      rel: "stylesheet",
-      href: "/src/style.css",
-    },
-  ],
+	title: "Bounce House Kings",
+	description: "Make your next party magical.",
+	url: "https://bounce-house-kings.com",
+	image: "/src/images/home/1.jpg",
+	links: [
+		{
+			rel: "icon",
+			href: "/src/images/global/favicon.png",
+		},
+		{
+			rel: "stylesheet",
+			href: "/src/style.css",
+		},
+	],
 };
